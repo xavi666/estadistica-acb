46a0027f29a0815490dc3886c1d076e2d6e53b9a
= <xavier@flowlens.com>
2016-10-07 09:01:50 +0100

    put init vars back

--- config/initializers/session_vars.rb
+++ config/initializers/session_vars.rb
@@ -1,2 +1,2 @@
-#CURRENT_SEASON    = Setting.find_by_key("current_season").try(:value) || "2016"
-#CURRENT_ROUND     = Setting.find_by_key("current_round").try(:value)  || "1"
+CURRENT_SEASON    = Setting.find_by_key("current_season").try(:value) || "2016"
+CURRENT_ROUND     = Setting.find_by_key("current_round").try(:value)  || "1"
