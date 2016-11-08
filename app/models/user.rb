5fc4d60c4e5132002b42cf13c2d5a325ab139f09
= <xavier@flowlens.com>
2016-11-08 13:00:44 +0000

    user devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,9 +39,8 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :recoverable, :rememberable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :trackable, :validatable
   # !**************************************************
   # !                Associations
   # !**************************************************
