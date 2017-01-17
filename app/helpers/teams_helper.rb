15ba306dcd977ee055bed4daf1d9ce334bd498e0
= <xavier@flowlens.com>
2017-01-17 16:36:14 +0000

    iconos

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,6 @@
 module TeamsHelper
 
   def team_image team, css = "team"
-    image_tag('teams/'+team.short_code+'.jpg', class: css).html_safe if team and team.short_code
+    image_tag('teams/'+team.short_code+'.png', class: css).html_safe if team and team.short_code
   end
 end
