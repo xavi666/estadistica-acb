55dd5f8774a287aa2e8ed871217b5f7f8551e776
Xavier <xavier@flowlens.com>
2017-02-14 19:06:06 +0000

    team image 22 height

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -3,4 +3,8 @@ module TeamsHelper
   def team_image team, css = "team"
     image_tag('teams/'+team.short_code+'.png', class: css).html_safe if team and team.short_code
   end
+
+  def team_image_22 team
+    image_tag('teams/height_22/'+team.short_code+'.png').html_safe if team and team.short_code
+  end
 end
