2fe3e81e7a861e6a74d1a5cc1fec8dc78eec6f20
= <xavier@flowlens.com>
2017-02-15 11:33:34 +0000

    assets

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,11 +1,24 @@
 module PlayersHelper
 
   def player_image player, css = "player"
-    image_tag(player.image.to_s, class: css) if player.image
+    if player.image
+      logical_path = 'players/original/'+player.image.to_s
+      if Player.asset_available? logical_path
+        image_tag('players/original/'+player.image.to_s, class: css).html_safe if player.image
+      else
+        image_tag('players/default.jpg', class: css).html_safe if player.image        
+      end
+    end
   end
 
-  def player_image_22 player
-    image_tag('players/height_22/'+player.image.to_s).html_safe
+  def player_image_22 player, css = "player"
+    if player.image
+      if Rails.application.assets.find_asset('players/height_22/'+player.image.to_s) != nil
+        image_tag('players/height_22/'+player.image.to_s, class: css).html_safe 
+      else
+        image_tag('players/default.jpg', class: css).html_safe if player.image
+      end
+    end
   end
 
   def player_price price
