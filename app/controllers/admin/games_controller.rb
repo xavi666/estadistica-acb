fc2f932ce3a7280c5dcf4a71ca4631d41a999d56
= <xavier@flowlens.com>
2017-02-17 17:39:02 +0000

    schedulling

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -39,50 +39,9 @@ class Admin::GamesController < ApplicationController
     Game.import_games
     redirect_to admin_games_path and return
   end
-
-  def import_game game
-    games_url = game.href
-    games_url.force_encoding('binary')
-    games_url = WEBrick::HTTPUtils.escape(games_url)
-    game_html = Nokogiri::HTML(open(games_url))
-
-    game_html.css("td.fichaJugadorData").each do |game_data|
-      position_detail = game_data.css('p[2]//text()').to_s
-      height = game_data.css('p[3]/strong[1]/text()').to_s
-      date_of_birth = game_data.css('p[5]/strong[1]/text()').to_s
-      place_of_birth = game_data.css('p[5]/strong[2]/text()').to_s
-      
-
-      game.position_detail = position_detail
-      game.height = height
-      game.date_of_birth = date_of_birth
-      game.place_of_birth = place_of_birth
-    end
-    game_html.css("td.fichaJugadorimg").each do |game_image|
-      image = Array.wrap(game_image.css("img").map { |link| link['src'] })[0].to_s
-      game.image = image
-    end
-    game.save!
-  end
-
+  
   def import_table
-    table_url = Setting.find_by_key("table_url").value
-    table_html = Nokogiri::HTML(open(table_url))
-
-    i = 0
-    table_html.css("table.resultados2 > tr").each do |game_row|
-      if i > 0
-        team_name = game_row.css('td[2]/a/text()').to_s
-        if team = Team.where("name = ? OR second_name = ?", team_name, team_name).first
-          team.position = game_row.css('td[1]//text()').to_s
-          team.played = game_row.css('td[3]//text()').to_s
-          team.won = game_row.css('td[4]//text()').to_s
-          team.lost = game_row.css('td[5]//text()').to_s
-          team.save!
-        end
-      end
-      i = i + 1
-    end
+    Team.import_table
     redirect_to admin_games_path and return
   end
 
@@ -92,6 +51,6 @@ class Admin::GamesController < ApplicationController
     end
 
     def game_params
-      params.require(:game).permit([:season, :round, :local_team_id, :away_team_id, :game_date])
+      params.require(:game).permit([:season, :round, :local_team_id, :away_team_id, :game_date, :local_score, :away_score])
     end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -34,4 +34,27 @@ class Team < ActiveRecord::Base
   def to_param
     [id.to_s, name.parameterize].join("-")
   end
+
+  # !**************************************************
+  # !                  Import
+  # !**************************************************  
+  def self.import_table
+    table_url = Setting.find_by_key("table_url").value
+    table_html = Nokogiri::HTML(open(table_url))
+
+    i = 0
+    table_html.css("table.resultados2 > tr").each do |game_row|
+      if i > 0
+        team_name = game_row.css('td[2]/a/text()').to_s
+        if team = Team.where("name = ? OR second_name = ?", team_name, team_name).first
+          team.position = game_row.css('td[1]//text()').to_s
+          team.played = game_row.css('td[3]//text()').to_s
+          team.won = game_row.css('td[4]//text()').to_s
+          team.lost = game_row.css('td[5]//text()').to_s
+          team.save!
+        end
+      end
+      i = i + 1
+    end
+  end
 end
