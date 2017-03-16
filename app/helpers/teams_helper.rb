cae959eadf899bc19858318737be1b4e3ff220e5
Xavier <xavier@flowlens.com>
2017-03-16 14:56:45 +0100

    original image

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -2,7 +2,7 @@ module TeamsHelper
 
   def team_image team, css = "team"
     if team and team.short_code
-      logical_path = 'teams/'+team.short_code+'.png'
+      logical_path = 'teams/original/'+team.short_code+'.png'
       if asset_available? logical_path
         image_tag(logical_path, class: css).html_safe
       else
